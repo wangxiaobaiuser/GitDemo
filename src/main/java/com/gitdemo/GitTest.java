a5fac3bdfd889e94bff54413078fb27b61101579
wang <1636925313@qq.com>
2023-01-13 14:42:44 +0800

    ThereCommit

--- src/main/java/com/gitdemo/GitTest.java
+++ src/main/java/com/gitdemo/GitTest.java
@@ -7,6 +7,6 @@ package com.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test02");
+        System.out.println("test03");
     }
 }
