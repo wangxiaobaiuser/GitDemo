b4aa0e24f566eebbcf6c03d0e870b8158e9b90d5
wang <1636925313@qq.com>
2023-01-13 13:58:59 +0800

    SecondCommit

---
+++ src/main/java/com/gitdemo/GitTest.java
@@ -0,0 +1,11 @@
+package com.gitdemo;
+
+/**
+ * @author wangAoJie
+ * 2023-01-13 13:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
